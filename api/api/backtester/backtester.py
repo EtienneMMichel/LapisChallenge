4add63779e81b04e6cb98c8d76800c887159efc2
Etienne <etienne.micheladj@gmail.com>
2024-07-23 17:30:06 +0200

    feat(api): complete optimizer backtester

---
+++ api/api/backtester/backtester.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from utils import DATA_PATH
+import os
+import json
+from datetime import datetime
+
+NB_MATCHES_BY_ALLOC = 5 # will be a request parameter in the future
+
+
+class Backtester():
+    def __init__(self) -> None:
+        pass
+
+    def fetch_data(self, sport, zone , competition):
+        with open(os.path.join(DATA_PATH, sport, zone, f"{competition}.json"), encoding='utf-8') as fh:
+            data = json.load(fh)
+        df = pd.DataFrame.from_records(data)
+        df["competition"] = [competition for _ in range(df.shape[0])]
+        df["zone"] = [zone for _ in range(df.shape[0])]
+        df["sport"] = [sport for _ in range(df.shape[0])]
+        return df
+
+    def retrieve_dataset(self, sport, start_year, end_year, zone=None, competitions=None):
+        dataset = None
+        sport_path = os.path.join(DATA_PATH, sport)
+        if not zone is None:
+            if competitions is None:
+                competitions = [comp[:-5] for comp in os.listdir(os.path.join(sport_path, zone))]
+            
+            for competition in competitions:
+                    df = self.fetch_data(sport, zone, competition)
+                    if dataset is None:
+                        dataset = df
+                    else:
+                        dataset = pd.concat([dataset, df], axis=0)
+            dataset.reset_index(inplace=True, drop=True)
+
+        else:
+            zones_availables = os.listdir(sport_path)
+            for zone in zones_availables:
+                sport_zone_path = os.path.join(sport_path, zone)
+                competitions_availables = [comp[:-5] for comp in os.listdir(sport_zone_path)]
+                for competition in competitions_availables:
+                    df = self.fetch_data(sport, zone, competition)
+                    if dataset is None:
+                        dataset = df
+                    else:
+                        dataset = pd.concat([dataset, df], axis=0)
+            dataset.reset_index(inplace=True, drop=True)
+
+        dataset["date"] = [pd.Timestamp(t, unit='s') for t in dataset["current_date"]]
+        start_date = datetime(start_year, 1,1,0,0)        
+        end_date = datetime(end_year, 1,1,0,0)   
+        dataset = dataset.loc[(dataset['date'] > start_date) & (dataset['date'] <= end_date)]
+
+        self.dataset = dataset.to_dict('records')
+
+    def dataset_gen(self):
+        i = NB_MATCHES_BY_ALLOC
+        while i < len(self.dataset):
+            yield self.dataset[i - NB_MATCHES_BY_ALLOC:i]
+            i += NB_MATCHES_BY_ALLOC
+
+                        
--- api/api/backtester/optimizer_backtester.py
+++ api/api/backtester/optimizer_backtester.py
@@ -1,12 +1,63 @@
-class OptimizerBacktester():
-    def __init__(self, forecaster_model, optimizer_model, sport, start_year, end_year, zones=None, competitions=None) -> None:
+from .backtester import Backtester
+from utils import PredsBody, AllocBody
+
+class OptimizerBacktester(Backtester):
+    def __init__(self, forecaster_model, optimizer_model, sport, start_year, end_year, zone=None, competitions=None) -> None:
         self.forecaster_model = forecaster_model
         self.optimizer_model = optimizer_model
         self.sport = sport
         self.start_year = start_year
         self.end_year = end_year
-        self.zones = zones
+        self.zone = zone
         self.competitions = competitions
+        self.retrieve_dataset(sport, start_year, end_year, zone, competitions)
+
+    def build_pred_input(self, match):
+        return PredsBody(team_1_id=match["team_1_name"],
+                         team_2_id=match["team_2_name"],
+                         date=match["current_date"],
+                         modelName="")
+    
+    def build_alloc_input(self, matches, preds):
+        return AllocBody(
+            preds=preds,
+            odds=[[match[odd_key] for odd_key in filter(lambda key: key[:4] == "odd_", list(match.keys()))] for match in matches]
+        )
+    
+    def reward(self, matches, portfolio_alloc):
+        winning_alloc_mapping = {
+            "1":0,
+            "2":1,
+            "null":2,
+        }
+        gain = 0
+        for match, portfolio_alloc_to_match in zip(matches, portfolio_alloc):
+            winning_odd = match[f"odd_team_{match['winner']}"]
+            winning_alloc_index = winning_alloc_mapping[match['winner']]
+            gain += winning_odd*portfolio_alloc_to_match[winning_alloc_index] - sum(portfolio_alloc_to_match)
+        return gain
+
 
     def process(self):
-        return {}
+        res = []
+        for matches in self.dataset_gen():
+            pred_inputs = [self.build_pred_input(match) for match in matches]
+            #2. make preds
+            preds = self.forecaster_model.act(pred_inputs)
+            #3. allocation
+            portfolios_alloc = {}
+            rewards = {}
+            for optimizer_model in self.optimizer_model:
+                alloc_inputs = self.build_alloc_input(matches, preds)
+                portfolio_alloc = optimizer_model.act(alloc_inputs)
+                portfolios_alloc[optimizer_model.name] = portfolio_alloc
+                rewards[optimizer_model.name] = self.reward(matches, portfolio_alloc)
+            res.append({
+                "date":matches[0]["date"],
+                "matches":matches,
+                "preds":preds,
+                "portfolios_alloc":portfolios_alloc,
+                "rewards":rewards
+            })
+        
+        return res
